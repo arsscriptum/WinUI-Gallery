3a187cf88f1b3f80b63e42b6d38aece6a855f583
Guillaume Plante <guillaume.plante@icloud.com>
2025-08-30 16:13:40 -0400

    auto

--- scripts/Get-LatestUpdatedSamples.ps1
+++ scripts/Get-LatestUpdatedSamples.ps1
@@ -2,11 +2,11 @@
 $SamplesFolder = Join-Path -Path $PSScriptRoot -ChildPath "..\WinUIGallery\Samples"
 
 # Change directory to the WinUIGallery folder
-Set-Location -Path $SamplesFolder
+Push-Location -Path $SamplesFolder
 
 # Define the folder containing the control pages
 $currentFolder = "ControlPages"
-
+$now = [datetimeoffset]::Now
 # Retrieve the list of files in the folder with their details
 $filesWithDetails = git ls-tree -r HEAD --name-only $currentFolder | ForEach-Object { 
     $file = $_  # Current file path from the git tree
@@ -20,13 +20,25 @@ $filesWithDetails = git ls-tree -r HEAD --name-only $currentFolder | ForEach-Obj
         $commitStatus = git log -1 --name-status -- $file | Select-String -Pattern "^M" | ForEach-Object { $_.Line.Split()[0] }
         # If the file was modified ("M"), create a custom object with file details
         if ($commitStatus -eq "M") {
+            $target = [datetimeoffset]::ParseExact($lastCommitDate, "yyyy-MM-dd HH:mm:ss zzz", $null)
+            $timespan = $now - $target
+            $Filename = $file.Substring($currentFolder.Length + 1)  # Trim the folder path to get the file name
+            $Displayname = $Filename -replace '\.xaml\.cs$', '' -replace '\.xaml$', ''
+            $Displayname = $Displayname -replace 'Page$', ''
+
+            $Fullpath = (Resolve-Path -Path "$file" -RelativeBasePath "$SamplesFolder").Path
             [PSCustomObject]@{
-                File = $file.Substring($currentFolder.Length + 1)  # Trim the folder path to get the file name
+                Fullpath = $Fullpath
+                RelativePath = $file
+                File = $Filename
+                Displayname = $Displayname
                 LastCommitDate = $lastCommitDate  # Last commit date for the file
+                Age = $timespan
             }
         }
     }
 }
+[System.Collections.ArrayList]$UpdatedList = [System.Collections.ArrayList]::new()
 # Sort the files by the last commit date in descending order
 $sortedFiles = $filesWithDetails | Sort-Object LastCommitDate -Descending
 # Create a hashtable to cache processed file base names to avoid duplicates
@@ -35,19 +47,32 @@ $cachedBaseNames = @{ }
 $LatestUpdatedSamples = "Latest Updated Samples:`n"
 # Process the sorted files
 $sortedFiles | ForEach-Object {
-    # Remove file extensions and standardize the file name
-    $fileName = $_.File -replace '\.xaml\.cs$', '' -replace '\.xaml$', ''
-    $fileName = $fileName -replace 'Page$', ''
+   
+    $name = $_.Displayname
     $date = $_.LastCommitDate
-
+    $days = $_.Age.Days
+    $color = 'DarkGreen'
+    if($days -lt 15){
+         $color = 'Green'
+    }elseif($days -lt 35){
+         $color = 'Yellow'
+    }elseif($days -lt 90){
+         $color = 'DarkRed'
+    }else{
+         $color = 'Magenta'
+    }
     # Add the file to the output if it hasn't been cached yet
-    if (-not $cachedBaseNames.Contains($fileName)) {
-        $cachedBaseNames[$fileName] = $true  # Mark the file name as cached
-        $LatestUpdatedSamples += $fileName + " (" + $date + ")`n" # Append the file name to the output
+    if (-not $cachedBaseNames.Contains($name)) {
+            $UpdateString = "{0,-32}`tUpdated {1} days ago" -f $name, $days
+        $LatestUpdatedSamples += "$UpdateString" # Append the file name to the output
+        Write-Host "$UpdateString" -f $color
     }
 }
 
+pop-location
+
 # Output the list of latest updated samples
-Write-Output $LatestUpdatedSamples
+#Write-Output $LatestUpdatedSamples
+
 # Wait for the user to press Enter before closing the PowerShell window
 Read-Host -Prompt "Press Enter to exit"
